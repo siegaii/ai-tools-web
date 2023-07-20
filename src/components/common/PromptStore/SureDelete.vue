1585d8d14487efc40a881fca712acfcdf9cebd8a
siegaii <siiegaiii@gmail.com>
2023-07-20 14:48:28 +0800

    feat: 提词器删除2次确认

---
+++ src/components/common/PromptStore/SureDelete.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import {
+  NButton,
+  NPopconfirm,
+} from 'naive-ui'
+import { t } from '@/locales'
+
+const props = defineProps(['onDelete'])
+
+const confirmDelete = () => {
+  props.onDelete()
+}
+
+const cancelDelete = () => {
+  // Do nothing for canceling the delete action
+}
+</script>
+
+<template>
+  <NPopconfirm @positive-click="confirmDelete" @negative-click="cancelDelete">
+    <template #trigger>
+      <NButton tertiary size="small" type="error">
+        {{ t("common.delete") }}
+      </NButton>
+    </template>
+    {{ t("common.confirmOperation") }}: {{ t("common.delete") }}
+  </NPopconfirm>
+</template>
