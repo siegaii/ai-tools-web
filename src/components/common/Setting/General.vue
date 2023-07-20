13c2402f3032941ffae77f851354af361957769a
siegaii <siiegaiii@gmail.com>
2023-07-20 11:41:02 +0800

    feat: 移除无效的国际化逻辑

--- src/components/common/Setting/General.vue
+++ src/components/common/Setting/General.vue
@@ -55,10 +55,7 @@ const themeOptions: { label: string; key: Theme; icon: string }[] = [
 
 const languageOptions: { label: string; key: Language; value: Language }[] = [
   { label: '简体中文', key: 'zh-CN', value: 'zh-CN' },
-  { label: '繁體中文', key: 'zh-TW', value: 'zh-TW' },
   { label: 'English', key: 'en-US', value: 'en-US' },
-  { label: '한국어', key: 'ko-KR', value: 'ko-KR' },
-  { label: 'Русский язык', key: 'ru-RU', value: 'ru-RU' },
 ]
 
 function updateUserInfo(options: Partial<UserInfo>) {
@@ -120,52 +117,78 @@ function handleImportButtonClick(): void {
   <div class="p-4 space-y-5 min-h-[200px]">
     <div class="space-y-6">
       <div class="flex items-center space-x-4">
-        <span class="flex-shrink-0 w-[100px]">{{ $t('setting.avatarLink') }}</span>
+        <span class="flex-shrink-0 w-[100px]">{{
+          $t("setting.avatarLink")
+        }}</span>
         <div class="flex-1">
           <NInput v-model:value="avatar" placeholder="" />
         </div>
-        <NButton size="tiny" text type="primary" @click="updateUserInfo({ avatar })">
-          {{ $t('common.save') }}
+        <NButton
+          size="tiny"
+          text
+          type="primary"
+          @click="updateUserInfo({ avatar })"
+        >
+          {{ $t("common.save") }}
         </NButton>
       </div>
       <div class="flex items-center space-x-4">
-        <span class="flex-shrink-0 w-[100px]">{{ $t('setting.name') }}</span>
+        <span class="flex-shrink-0 w-[100px]">{{ $t("setting.name") }}</span>
         <div class="w-[200px]">
           <NInput v-model:value="name" placeholder="" />
         </div>
-        <NButton size="tiny" text type="primary" @click="updateUserInfo({ name })">
-          {{ $t('common.save') }}
+        <NButton
+          size="tiny"
+          text
+          type="primary"
+          @click="updateUserInfo({ name })"
+        >
+          {{ $t("common.save") }}
         </NButton>
       </div>
       <div class="flex items-center space-x-4">
-        <span class="flex-shrink-0 w-[100px]">{{ $t('setting.description') }}</span>
+        <span class="flex-shrink-0 w-[100px]">{{
+          $t("setting.description")
+        }}</span>
         <div class="flex-1">
           <NInput v-model:value="description" placeholder="" />
         </div>
-        <NButton size="tiny" text type="primary" @click="updateUserInfo({ description })">
-          {{ $t('common.save') }}
+        <NButton
+          size="tiny"
+          text
+          type="primary"
+          @click="updateUserInfo({ description })"
+        >
+          {{ $t("common.save") }}
         </NButton>
       </div>
       <div
         class="flex items-center space-x-4"
         :class="isMobile && 'items-start'"
       >
-        <span class="flex-shrink-0 w-[100px]">{{ $t('setting.chatHistory') }}</span>
+        <span class="flex-shrink-0 w-[100px]">{{
+          $t("setting.chatHistory")
+        }}</span>
 
         <div class="flex flex-wrap items-center gap-4">
           <NButton size="small" @click="exportData">
             <template #icon>
               <SvgIcon icon="ri:download-2-fill" />
             </template>
-            {{ $t('common.export') }}
+            {{ $t("common.export") }}
           </NButton>
 
-          <input id="fileInput" type="file" style="display:none" @change="importData">
+          <input
+            id="fileInput"
+            type="file"
+            style="display: none"
+            @change="importData"
+          >
           <NButton size="small" @click="handleImportButtonClick">
             <template #icon>
               <SvgIcon icon="ri:upload-2-fill" />
             </template>
-            {{ $t('common.import') }}
+            {{ $t("common.import") }}
           </NButton>
 
           <NPopconfirm placement="bottom" @positive-click="clearData">
@@ -174,15 +197,15 @@ function handleImportButtonClick(): void {
                 <template #icon>
                   <SvgIcon icon="ri:close-circle-line" />
                 </template>
-                {{ $t('common.clear') }}
+                {{ $t("common.clear") }}
               </NButton>
             </template>
-            {{ $t('chat.clearHistoryConfirm') }}
+            {{ $t("chat.clearHistoryConfirm") }}
           </NPopconfirm>
         </div>
       </div>
       <div class="flex items-center space-x-4">
-        <span class="flex-shrink-0 w-[100px]">{{ $t('setting.theme') }}</span>
+        <span class="flex-shrink-0 w-[100px]">{{ $t("setting.theme") }}</span>
         <div class="flex flex-wrap items-center gap-4">
           <template v-for="item of themeOptions" :key="item.key">
             <NButton
@@ -198,13 +221,15 @@ function handleImportButtonClick(): void {
         </div>
       </div>
       <div class="flex items-center space-x-4">
-        <span class="flex-shrink-0 w-[100px]">{{ $t('setting.language') }}</span>
+        <span class="flex-shrink-0 w-[100px]">{{
+          $t("setting.language")
+        }}</span>
         <div class="flex flex-wrap items-center gap-4">
           <NSelect
             style="width: 140px"
             :value="language"
             :options="languageOptions"
-            @update-value="value => appStore.setLanguage(value)"
+            @update-value="(value) => appStore.setLanguage(value)"
           />
         </div>
       </div>
