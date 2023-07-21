5b2ff29e6f0fb1df1700e320b2f383464f38ed68
siegaii <siiegaiii@gmail.com>
2023-07-21 18:36:38 +0800

    feat: 删除无效接口

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,18 +2,6 @@ import type { AxiosProgressEvent, GenericAbortSignal } from 'axios'
 import { post } from '@/utils/request'
 import { useAuthStore, useSettingStore } from '@/store'
 
-export function fetchChatAPI<T = any>(
-  prompt: string,
-  options?: { conversationId?: string; parentMessageId?: string },
-  signal?: GenericAbortSignal,
-) {
-  return post<T>({
-    url: '/chat',
-    data: { prompt, options },
-    signal,
-  })
-}
-
 export function fetchChatConfig<T = any>() {
   return post<T>({
     url: '/config',
@@ -49,12 +37,5 @@ export function fetchChatAPIProcess<T = any>(
     data,
     signal: params.signal,
     onDownloadProgress: params.onDownloadProgress,
-  })
-}
-
-export function fetchVerify<T>(token: string) {
-  return post<T>({
-    url: '/verify',
-    data: { token },
   })
 }
